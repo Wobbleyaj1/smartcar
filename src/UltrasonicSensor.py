eb10fd70ce46fe622d89dac16f1b29dcf6134701
Jay Nipper <jessenipper@gmail.com>
2025-02-07 14:51:25 -0500

    Update UltrasonicSensor.py to implement distance measurement using GPIO and handle user interrupts

--- src/UltrasonicSensor.py
+++ src/UltrasonicSensor.py
@@ -1,8 +1,36 @@
-from gpiozero import DistanceSensor
+import time
+import RPi.GPIO as GPIO
 
-ultrasonic = DistanceSensor(echo=17, trigger=4, threshold_distance=0.2)
-while True:
-    ultrasonic.wait_for_out_of_range()
-        # Go car
-    ultrasonic.wait_for_in_range()
-        # Stop car
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23
+ECHO = 24
+
+print("Distance measurement in progress")
+
+GPIO.setup(TRIG, GPIO.OUT)  
+GPIO.setup(ECHO, GPIO.IN)
+
+try:
+    while True:
+        GPIO.output(TRIG, False)
+        time.sleep(.5)
+
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        print("Distance:", distance, "cm")
+
+except KeyboardInterrupt:
+    print("Measurement stopped by user")
+    GPIO.cleanup()
