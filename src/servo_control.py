1a5eb22fff24ddbf5f202d2a838edab208ac9153
Jay Nipper <jessenipper@gmail.com>
2025-02-07 15:43:37 -0500

    Implement keyboard control for servo motor in servo_control.py

--- src/servo_control.py
+++ src/servo_control.py
@@ -1,7 +1,6 @@
-# We imports the GPIO module
 import RPi.GPIO as GPIO
-# We import the command sleep from time
 from time import sleep
+import keyboard
 
 # Stops all warnings from appearing
 GPIO.setwarnings(False)
@@ -13,12 +12,24 @@ GPIO.setup(16, GPIO.OUT)
 
 # Set up the PWM on pin #16 at 50Hz
 pwm = GPIO.PWM(16, 50)
-pwm.start(0) # Start the servo with 0 duty cycle ( at 0 deg position )
-pwm.ChangeDutyCycle(5) # Tells the servo to turn to the left ( -90 deg position )
-sleep(0.5) # Tells the servo to Delay for 5sec
-pwm.ChangeDutyCycle(7.5) # Tells the servo to turn to the neutral position ( at 0 deg position )
-sleep(0.5) # Tells the servo to Delay for 5sec
-pwm.ChangeDutyCycle(10) # Tells the servo to turn to the right ( +90 deg position )
-sleep(0.5) # Tells the servo to Delay for 5sec
-pwm.stop(0) # Stop the servo with 0 duty cycle ( at 0 deg position )
-GPIO.cleanup() # Clean up all the ports we've used.
+pwm.start(7.5) # Start the servo at neutral position (0 deg position)
+
+try:
+    duty_cycle = 7.5 # Neutral position
+    while True:
+        if keyboard.is_pressed('up'):
+            duty_cycle += 0.1 # Increase duty cycle to turn right
+            if duty_cycle > 12.5: # Limit to +90 deg position
+                duty_cycle = 12.5
+            pwm.ChangeDutyCycle(duty_cycle)
+        elif keyboard.is_pressed('down'):
+            duty_cycle -= 0.1 # Decrease duty cycle to turn left
+            if duty_cycle < 2.5: # Limit to -90 deg position
+                duty_cycle = 2.5
+            pwm.ChangeDutyCycle(duty_cycle)
+        sleep(0.1) # Small delay to control the speed of turning
+except KeyboardInterrupt:
+    pass
+finally:
+    pwm.stop() # Stop the servo
+    GPIO.cleanup() # Clean up all the ports we've used
