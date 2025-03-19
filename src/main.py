5296100e8f6f5222090eb161057fcc2b88be9471
Sekou <sekagard@gmail.com>
2025-03-19 16:33:37 -0400

    updating main and boundary detection

--- src/main.py
+++ src/main.py
@@ -2,31 +2,34 @@ import threading
 import signal
 from servo_control import ServoControl
 from ultrasonic_sensor import UltrasonicSensor
-from qtr_sensor import QTRSensorThread 
+from boundary_detection import QTRSensorThread 
 
 def main():
     stop_event = threading.Event()
 
     servo_control = ServoControl(stop_event)
     ultrasonic_sensor = UltrasonicSensor(stop_event)
+    qtr_sensor= QTRSensorThread(stop_event)
 
     servo_thread = threading.Thread(target=servo_control.run)
     ultrasonic_thread = threading.Thread(target=ultrasonic_sensor.run)
-
+    qtr_thread = threading.Thread(target=qtr_sensor.run)  # <-- Add QTR thread
     def signal_handler(sig, frame):
         print("KeyboardInterrupt received, stopping threads...")
         stop_event.set()
         servo_thread.join()
         ultrasonic_thread.join()
+        qtr_thread.join()
         print("Threads stopped, exiting program.")
 
     signal.signal(signal.SIGINT, signal_handler)
 
     servo_thread.start()
     ultrasonic_thread.start()
+    qtr_thread.start()
 
     servo_thread.join()
     ultrasonic_thread.join()
-
+    qtr_thread.join()
 if __name__ == "__main__":
     main()
