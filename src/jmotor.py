717c3eda3f36dc93a65fb07c44440a2cb89ec8fc
Jay Nipper <jessenipper@gmail.com>
2025-04-10 14:11:48 -0400

    Refactor cleanup calls in jmotor.py and jmovement.py for consistency

--- src/jmotor.py
+++ src/jmotor.py
@@ -87,5 +87,3 @@ if __name__ == "__main__":
         print("\nExiting program.")
     finally:
         motor.stop_motor()
-
-io.cleanup()
--- src/jmovement.py
+++ src/jmovement.py
@@ -93,4 +93,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\nExiting program.")
     finally:
-        movement.cleanup()    
+        movement.cleanup()
+
+    
+    io.cleanup()
